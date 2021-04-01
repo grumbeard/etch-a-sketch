e3f0484bfbf1e01d9d6ca5cdb68a60bf6bb42073
Kaiying Fu <kaiying.fu@gmail.com>
2021-04-01 15:30:00 +0800

    Add feature to vary grid size by square count while maintaining square dimentions

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,29 @@
 const gridContainer = document.getElementById("grid-container");
 
+
 // Add 16 x 16 cells to create grid
-for (let i = 0; i < (16*16); i++) {
-    // Create a single cell element
-    let gridCell = document.createElement("div");
-    gridCell.className="grid-cell";
-    gridContainer.appendChild(gridCell);
+paintGrid(16);
+
+// Define 'paint grid' feature to create grid with given dimensions
+function paintGrid(squareCount) {
+    // Remove all cells in grid
+    gridContainer.innerHTML = "";
+
+    // Recreate cells in grid
+    for (let i = 0; i < squareCount; i++) {
+        /* Create a single role of cell elements
+            Creating cells row by row allows cells to flex to equal width and height*/
+        let gridRow = document.createElement("div");
+        gridRow.className="grid-row";
+        for (let j = 0; j < squareCount; j++) {
+            // Create cell elements to add to row
+            let gridCell = document.createElement("div");
+            gridCell.className="grid-cell";
+            gridRow.appendChild(gridCell);
+        }
+        // Add single row of cell elements to grid
+        gridContainer.appendChild(gridRow);
+    }
 }
 
 // Add 'hover' effect to grid cells
@@ -16,20 +34,32 @@ function handleMouseover(e) {
     if (!classes.contains("color-me")) classes.add("color-me");
 }
 
+
 // Add 'reset' functionality through button "reset-btn"
 const resetButton = document.getElementById("reset-btn");
 resetButton.addEventListener('click', handleReset);
 
+function handleReset() {
+    removeColor(gridCells);
+    resetGridSquareCount();
+}
+
 // Use querySelector to obtain a static NodeList to iterate over for removal of color
 const gridCells = document.querySelectorAll(".grid-cell");
 
-function handleReset() {
-    removeColor(gridCells);
-}
-
+// Define 'remove color' feature for erasing all drawing on grid
 function removeColor(cells) {
     cells.forEach(cell => {
         let classes = cell.classList;
         if (classes.contains("color-me")) classes.remove("color-me");
     });
+}
+
+// Define 'reset grid square count' feature for repainting grid with new dimensions
+function resetGridSquareCount() {
+    let squareCount = 0;
+    while (squareCount < 1 || squareCount > 100) {
+        squareCount = prompt("How many squares?");
+    }
+    paintGrid(squareCount);
 }
